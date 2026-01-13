4c1ca4a54cbb268cb85ce08a08020bda38db5d2d
Alexandr Romaniuc <alexromaniuc@gmail.com>
2026-01-13 20:19:55 +0200

    first commit

--- src/main/java/com/example/Cat.java
+++ src/main/java/com/example/Cat.java
@@ -3,14 +3,17 @@ package com.example;
 import java.util.List;
 
 public class Cat {
+    //1. Создать интерфейс для класса-зависимости. - Predator
+    //2. Создать в зависимом классе поле того же типа, что и интерфейс.
+    private final Predator predator;
 
-    Predator predator;
-
-    public Cat(Feline feline) {
-        this.predator = feline;
+    //3. Создать конструктор с параметром того же типа, что и интерфейс
+    public Cat(Predator predator) {
+        this.predator = predator;
     }
 
     public String getSound() {
+
         return "Мяу";
     }
 
